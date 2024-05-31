af020ec7343b85fa7455d189eb78602d5f1e3d39
Odael <isakovodael@gmail.com>
2024-05-31 21:36:16 +0300

    Change colors on click in winner screen

--- menus/winner_screen.py
+++ menus/winner_screen.py
@@ -1,15 +1,27 @@
 import turtle
 
+from helpers.utils import pick_random_color
+
+
+def change_text_color(text='', x=None, y=None):
+    new_text_color = pick_random_color()
+    turtle.color(new_text_color)
+    turtle.write(text, align="center", font=("Impact", 70))
+
 
 def display_winner_screen(winner_name):
     turtle.TurtleScreen._RUNNING = True
-    turtle.color("yellow")
     winner_screen = turtle.Screen()
     winner_screen.bgcolor("black")
     winner_screen.setup(width=1.0, height=1.0)
 
+    turtle.colormode(255)
+
+    text_to_show = f"{winner_name} won!"
+    change_text_color(text_to_show)
+    winner_screen.onclick(lambda x, y: change_text_color(text_to_show, x, y))
+
     print("winner:", winner_name)
-    turtle.write(f"{winner_name} won!", align="center", font=("Impact", 70))
     turtle.hideturtle()
 
     winner_screen.mainloop()
