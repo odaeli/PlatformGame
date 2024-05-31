567b389982a1e86516515b90debbbcd70d5c9ca8
Odael <isakovodael@gmail.com>
2024-05-31 21:46:43 +0300

    Make colors change automatically in winner screen

--- menus/winner_screen.py
+++ menus/winner_screen.py
@@ -1,25 +1,32 @@
 import turtle
+from time import sleep
 
 from helpers.utils import pick_random_color
 
+winner_screen = None
 
-def change_text_color(text='', x=None, y=None):
+
+def change_text_color(text='', x=None, y=None, sleep_time=0):
+    if sleep_time > 0:
+        sleep(sleep_time)
     new_text_color = pick_random_color()
     turtle.color(new_text_color)
     turtle.write(text, align="center", font=("Impact", 70))
+    winner_screen.ontimer(change_text_color(text, x, y, 0.5))
 
 
 def display_winner_screen(winner_name):
+    global winner_screen
     turtle.TurtleScreen._RUNNING = True
     winner_screen = turtle.Screen()
     winner_screen.bgcolor("black")
     winner_screen.setup(width=1.0, height=1.0)
 
     turtle.colormode(255)
+    turtle.hideturtle()
 
     text_to_show = f"{winner_name} won!"
     change_text_color(text_to_show)
-    winner_screen.onclick(lambda x, y: change_text_color(text_to_show, x, y))
 
     print("winner:", winner_name)
     turtle.hideturtle()
